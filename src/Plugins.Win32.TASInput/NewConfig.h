d18bc879ba3801ca15bcecb7e9d11e707f6300b2
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2026-02-03 20:00:32 +0100

    fix(TASInput): selecting keyboard preset causes crash (#480)

--- src/Plugins.Win32.TASInput/NewConfig.h
+++ src/Plugins.Win32.TASInput/NewConfig.h
@@ -22,8 +22,6 @@ struct t_button_mapping
     int32_t button = SDL_GAMEPAD_BUTTON_INVALID;
     int32_t axis = SDL_GAMEPAD_AXIS_INVALID;
     int32_t key = 0;
-
-    static t_button_mapping from_button(int32_t button) { return {button, SDL_GAMEPAD_AXIS_INVALID, 0}; }
 };
 
 struct t_controller_config
@@ -38,15 +36,15 @@ struct t_controller_config
     t_button_mapping c_down{};
     t_button_mapping c_up{};
 
-    t_button_mapping a = t_button_mapping::from_button(SDL_GAMEPAD_BUTTON_SOUTH);
-    t_button_mapping b = t_button_mapping::from_button(SDL_GAMEPAD_BUTTON_EAST);
-    t_button_mapping z = t_button_mapping::from_button(SDL_GAMEPAD_BUTTON_WEST);
-    t_button_mapping start = t_button_mapping::from_button(SDL_GAMEPAD_BUTTON_START);
-    t_button_mapping l = t_button_mapping::from_button(SDL_GAMEPAD_BUTTON_LEFT_SHOULDER);
-    t_button_mapping r = t_button_mapping::from_button(SDL_GAMEPAD_BUTTON_RIGHT_SHOULDER);
+    t_button_mapping a = {.button = SDL_GAMEPAD_BUTTON_SOUTH};
+    t_button_mapping b = {.button = SDL_GAMEPAD_BUTTON_EAST};
+    t_button_mapping z = {.button = SDL_GAMEPAD_BUTTON_WEST};
+    t_button_mapping start = {.button = SDL_GAMEPAD_BUTTON_START};
+    t_button_mapping l = {.button = SDL_GAMEPAD_BUTTON_LEFT_SHOULDER};
+    t_button_mapping r = {.button = SDL_GAMEPAD_BUTTON_RIGHT_SHOULDER};
 
-    t_axis_mapping x = {SDL_GAMEPAD_AXIS_LEFTX, 0, 0};
-    t_axis_mapping y = {SDL_GAMEPAD_AXIS_LEFTY, 0, 0};
+    t_axis_mapping x = {.axis = SDL_GAMEPAD_AXIS_LEFTX};
+    t_axis_mapping y = {.axis = SDL_GAMEPAD_AXIS_LEFTY};
 
     float x_scale = 1.0f;
     float y_scale = 1.0f;
@@ -54,22 +52,22 @@ struct t_controller_config
     static t_controller_config keyboard_config()
     {
         t_controller_config config{};
-        config.a = {SDL_GAMEPAD_BUTTON_INVALID, 'X'};
-        config.b = {SDL_GAMEPAD_BUTTON_INVALID, 'Z'};
-        config.start = {SDL_GAMEPAD_BUTTON_INVALID, VK_RETURN};
-        config.z = {SDL_GAMEPAD_BUTTON_INVALID, 'A'};
-        config.l = {SDL_GAMEPAD_BUTTON_INVALID, 'S'};
-        config.r = {SDL_GAMEPAD_BUTTON_INVALID, 'D'};
-        config.dpad_up = {SDL_GAMEPAD_BUTTON_INVALID, VK_UP};
-        config.dpad_down = {SDL_GAMEPAD_BUTTON_INVALID, VK_DOWN};
-        config.dpad_left = {SDL_GAMEPAD_BUTTON_INVALID, VK_LEFT};
-        config.dpad_right = {SDL_GAMEPAD_BUTTON_INVALID, VK_RIGHT};
-        config.c_up = {SDL_GAMEPAD_BUTTON_INVALID, 'W'};
-        config.c_down = {SDL_GAMEPAD_BUTTON_INVALID, 'Q'};
-        config.c_left = {SDL_GAMEPAD_BUTTON_INVALID, 'E'};
-        config.c_right = {SDL_GAMEPAD_BUTTON_INVALID, 'R'};
-        config.x = {SDL_GAMEPAD_AXIS_INVALID, 'J', 'L'};
-        config.y = {SDL_GAMEPAD_AXIS_INVALID, 'I', 'K'};
+        config.a = {.key = 'X'};
+        config.b = {.key = 'Z'};
+        config.start = {.key = VK_RETURN};
+        config.z = {.key = 'A'};
+        config.l = {.key = 'S'};
+        config.r = {.key = 'D'};
+        config.dpad_up = {.key = VK_UP};
+        config.dpad_down = {.key = VK_DOWN};
+        config.dpad_left = {.key = VK_LEFT};
+        config.dpad_right = {.key = VK_RIGHT};
+        config.c_up = {.key = 'W'};
+        config.c_down = {.key = 'Q'};
+        config.c_left = {.key = 'E'};
+        config.c_right = {.key = 'R'};
+        config.x = {.key_negative = 'J', .key_positive = 'L'};
+        config.y = {.key_negative = 'I', .key_positive = 'K'};
         return config;
     }
 };
